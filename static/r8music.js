f097dbe98f4999b2d956373c15db4cfd9babef17
Fedjmike <sam.nipps@gmail.com>
2016-02-10 22:50:49 +0000

    Polished rating interaction
    - Moved the code to its own JS file
    - Update the rating widget when ACK received
    - But only if ACK

---
+++ static/r8music.js
@@ -0,0 +1,18 @@
+function rateRelease(clicked_element, release_id, rating) {
+    $.ajax({
+        method: "POST",
+        url: "/rate/" + release_id + "/" + rating
+        
+    }).done(function (msg) {
+        if (msg != "ok") {
+            return;
+        }
+        
+        var siblings = clicked_element.parentNode.children;
+        
+        for (var i = 0; i < siblings.length; i++)
+            siblings[i].classList.remove("selected");
+            
+        clicked_element.classList.add("selected");
+    });
+}
