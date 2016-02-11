44e286cf164a8528b3e88d457aa85598f7c82d6a
Fedjmike <sam.nipps@gmail.com>
2016-02-11 00:25:11 +0000

    Click the current rating to unrate the release

--- static/r8music.js
+++ static/r8music.js
@@ -1,4 +1,24 @@
+function unrateRelease(clicked_element, release_id) {
+    $.ajax({
+        method: "POST",
+        url: "/unrate/" + release_id
+        
+    }).done(function (msg) {
+        if (msg != "ok")
+            return;
+            
+        clicked_element.classList.remove("selected");
+    })
+}
+
 function rateRelease(clicked_element, release_id, rating) {
+    /*User clicked the already selected rating
+       => unrate*/
+    if (clicked_element.classList.contains("selected")) {
+        unrateRelease(clicked_element, release_id);
+        return;
+    }
+    
     $.ajax({
         method: "POST",
         url: "/rate/" + release_id + "/" + rating
@@ -6,6 +26,8 @@ function rateRelease(clicked_element, release_id, rating) {
     }).done(function (msg) {
         if (msg != "ok")
             return;
+            
+        /*Success, update rating widget*/
         
         var siblings = clicked_element.parentNode.children;
         
