25fa9cb0b8a330ddc126db83432b694b99017776
Satyarth Mishra Sharma <mail@satyarth.me>
2016-02-14 06:34:19 +0300

    Convert tabs to spaces :skull:

--- mb_db_import.py
+++ mb_db_import.py
@@ -9,16 +9,16 @@ def gid_from_name(name):
     musicbrainzngs.set_useragent("Skiller", "0.0.0", "mb@satyarth.me")
     result = musicbrainzngs.search_artists(artist=artist_name)
     gid = result['artist-list'][0]['id']
-	return gid
+    return gid
 
 def import_artist(gid):
     mb_db = psycopg2.connect(user=user, database=database, password=password)
-	cur = mb_db.cursor()
-	(_id,) = cur.execute("select id from artist where gid = %s", (gid,))
-	print(_id)
+    cur = mb_db.cursor()
+    (_id,) = cur.execute("select id from artist where gid = %s", (gid,))
+    print(_id)
 
 
 if __name__ == '__main__':
-	gid = gid_from_name(sys.argv[1])
+    gid = gid_from_name(sys.argv[1])
     import_artist(gid)
 
