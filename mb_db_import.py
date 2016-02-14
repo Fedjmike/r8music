fe27fc5e36d7a7f1a640224c29f0c1543b088dff
Satyarth Mishra Sharma <mail@satyarth.me>
2016-02-14 06:28:39 +0300

    minor update

--- mb_db_import.py
+++ mb_db_import.py
@@ -4,12 +4,21 @@ import sys
 from config import database, user, password
 
 
-conn = psycopg2.connect(user=user, database=database, password=password)
-cur = conn.cursor()
+def gid_from_name(name):
+    print("Querying MB for artist info...")
+    musicbrainzngs.set_useragent("Skiller", "0.0.0", "mb@satyarth.me")
+    result = musicbrainzngs.search_artists(artist=artist_name)
+    gid = result['artist-list'][0]['id']
+	return gid
 
-def import_artist:
-    pass
+def import_artist(gid):
+    mb_db = psycopg2.connect(user=user, database=database, password=password)
+	cur = mb_db.cursor()
+	(_id,) = cur.execute("select id from artist where gid = %s", (gid,))
+	print(_id)
+
 
 if __name__ == '__main__':
-    import_artist(sys.argv[1])
+	gid = gid_from_name(sys.argv[1])
+    import_artist(gid)
 
