4eb30583eb47814ec9a072b5c8a2af77c805890e
Satyarth Mishra Sharma <janky@satyarth.me>
2016-02-14 19:01:03 -0500

    Get release ids for each release group after sorting by date

--- mb_db_import.py
+++ mb_db_import.py
@@ -11,6 +11,11 @@ def gid_from_name(artist_name):
     print(gid)
     return gid
 
+# TODO:
+    # Finish writing DB calls
+    # Merge DB calls
+    # Populate local database
+    # Dupe avoidance
 def import_artist(gid):
     mb_db = psycopg2.connect(user=user, database=database, password=password)
     cur = mb_db.cursor()
@@ -26,13 +31,10 @@ def import_artist(gid):
         for (release_group_id,) in cur.fetchall():
             release_group_ids.append(release_group_id)
     print(release_group_ids)
+    release_ids = []
     for release_group_id in release_group_ids:
-        cur.execute("select id from release where release_group = %s", (release_group_id,))
-        release_ids = [_id for (_id,) in cur.fetchall()]
-        print(release_ids)
-        for release_id in release_ids:
-            cur.execute("select * from release_country where release = %s", (release_id,))
-
+        cur.execute("select id from (select id from release where release_group = %s) r left join release_country c on r.id = c.release order by date_year nulls last, date_month nulls last, date_day nulls last limit 1", (release_group_id,))
+        release_ids.append(cur.fetchone()[0])
 
 if __name__ == '__main__':
     gid = gid_from_name(sys.argv[1])
