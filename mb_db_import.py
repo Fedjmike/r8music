c0b1b1af23a4ed5180b3790bd45e1f09a97f6ced
Satyarth Mishra Sharma <janky@satyarth.me>
2016-02-16 01:12:09 -0500

    Pre retabling commit

--- mb_db_import.py
+++ mb_db_import.py
@@ -1,6 +1,7 @@
 import musicbrainzngs
 import psycopg2
 import sys, sqlite3
+import import_tools
 from config import database, user, password
 
 def gid_from_name(artist_name):
@@ -21,10 +22,11 @@ def import_artist(gid):
     lcr = local_db.cursor()
     mb_db = psycopg2.connect(user=user, database=database, password=password)
     cr = mb_db.cursor()
-    cr.execute("SELECT id FROM artist WHERE gid = %s", (gid,))
-    (_id,) = cr.fetchone()
-    print(_id)
-    cr.execute("SELECT artist_credit FROM artist_credit_name WHERE artist = %s", (_id,))
+    cr.execute("SELECT id, name FROM artist WHERE gid = %s", (gid,))
+    artist = {}
+    ((artist['id'], artist['name']),) = cr.fetchall()
+    print(artist['id'])
+    cr.execute("SELECT artist_credit FROM artist_credit_name WHERE artist = %s", (artist['id'],))
     artist_credit_ids = [ac_id for (ac_id,) in cr.fetchall()]
     print(artist_credit_ids)
     release_group_ids = []
@@ -39,8 +41,27 @@ def import_artist(gid):
         cr.execute("select id from (select id from release where release_group = %s) r left join release_country c on r.id = c.release order by date_year nulls last, date_month nulls last, date_day nulls last limit 1", (release_group_id,))
         release_ids.append(cr.fetchone()[0])
     print(release_ids)
-    
+    releases = []
+    for release_id in release_ids:
+        release = {}
+        cr.execute("select position, name, length from track where medium = (select id from medium where release = %s order by position limit 1)", (release_id,))
+        tracks = cr.fetchall()
+        print(tracks)
+        release['tracks'] = tracks
 
+        cr.execute("select cover_art_url from release_coverart where id = %s", (release_id,))
+        try:
+            # TODO: Actually get the cover art url lel
+            (release['cover_art_url'],) = cr.fetchone()
+        except TypeError:
+            release['cover_art_url'] = None
+        print(release['cover_art_url'])
+    #lcr.execute(
+     #   "insert into artists (name, slug, incomplete) values (?, ?, ?)",
+      #  (artist['name'], import_tools.slugify(artist['name']), None)
+    #)
+
+    local_db.commit()
 
 if __name__ == '__main__':
     gid = gid_from_name(sys.argv[1])
