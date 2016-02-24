f55e2fadca6f903e06cac32df4dd91c56bf04ce4
Fedjmike <sam.nipps@gmail.com>
2016-02-24 02:10:27 +0000

    Guess the wikipedia page if not given by musicbrainz

--- mb_api_import.py
+++ mb_api_import.py
@@ -111,6 +111,11 @@ def import_artist(artist_name):
     links = get_links(artist_mbid)
     for link_type, target in links.items():
         model.add_artist_link(artist_id, link_type, target)
+            
+    if "wikipedia" not in links:
+        print("Guessing wikipedia link...")
+        title = import_tools.guess_wikipedia_page(artist_name)
+        model.add_artist_link(artist_id, "wikipedia", title)
 
     pool = ThreadPool(8)
     releases = get_releases(artist_mbid, processed_release_mbids)
