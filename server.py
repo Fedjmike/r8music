6b9292a3b3339a82dd481f7d36edd8d0da7e5a28
Satyarth Mishra Sharma <janky@satyarth.me>
2016-12-22 16:07:22 -0500

    serve over https, prettyprint server activity

--- server.py
+++ server.py
@@ -1,8 +1,17 @@
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
+from tornado.options import options
 from website import app
+from config import CERT_PATH, KEY_PATH
 
+options.parse_command_line(args=None, final=True)
+https_server = HTTPServer(WSGIContainer(app), ssl_options=
+    {
+        "certfile": CERT_PATH,
+        "keyfile": KEY_PATH
+    })
 http_server = HTTPServer(WSGIContainer(app))
 http_server.listen(80)
+https_server.listen(443)
 IOLoop.instance().start()
