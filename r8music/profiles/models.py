ab61f01001c79df573dce5a037af5906241a4557
Fedjmike <sam.nipps@gmail.com>
2019-05-21 20:10:07 +0100

    Change various foreign keys to "cascade" deletes

--- r8music/profiles/models.py
+++ r8music/profiles/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserSettings(models.Model):
-    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name="settings")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="settings")
     #Stored as a UTC offset, in the format "[+-]\d\d:\d\d"
     timezone = models.TextField()
     #Does 'listening' to a release automatically remove it from the 'saved' list?
@@ -11,16 +11,16 @@ class UserSettings(models.Model):
 #
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.PROTECT, related_name="profile")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     avatar_url = models.TextField()
     
 class UserRatingDescription(models.Model):
     """The description of this rating displayed on the user's profile page"""
-    user = models.ForeignKey(UserProfile, on_delete=models.PROTECT, related_name="rating_descriptions")
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="rating_descriptions")
     rating = models.IntegerField()
     description = models.TextField()
     
 class Followership(models.Model):
-    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="followers")
-    follower = models.ForeignKey(User, on_delete=models.PROTECT, related_name="following")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followers")
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
     creation = models.DateTimeField(auto_now_add=True)
--- r8music/v1transfer/models.py
+++ r8music/v1transfer/models.py
@@ -7,28 +7,28 @@ from r8music.actions.models import Action
 #These tables link objects with those they were imported from in the V1 database
 
 class UserV1Link(models.Model):
-    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     old_id = models.IntegerField(unique=True)
     #If this user still uses a password created in V1,
     #the (Flask created) hash is stored here.
     password_hash = models.TextField(null=True)
     
 class TagV1Link(models.Model):
-    tag = models.OneToOneField(Tag, on_delete=models.PROTECT)
+    tag = models.OneToOneField(Tag, on_delete=models.CASCADE)
     old_id = models.IntegerField(unique=True)
 
 class ArtistV1Link(models.Model):
-    artist = models.OneToOneField(Artist, on_delete=models.PROTECT)
+    artist = models.OneToOneField(Artist, on_delete=models.CASCADE)
     old_id = models.IntegerField(unique=True)
     
 class ReleaseV1Link(models.Model):
-    release = models.OneToOneField(Release, on_delete=models.PROTECT)
+    release = models.OneToOneField(Release, on_delete=models.CASCADE)
     old_id = models.IntegerField(unique=True)
     
 class TrackV1Link(models.Model):
-    track = models.OneToOneField(Track, on_delete=models.PROTECT)
+    track = models.OneToOneField(Track, on_delete=models.CASCADE)
     old_id = models.IntegerField(unique=True)
 
 class ActionV1Link(models.Model):
-    action = models.OneToOneField(Action, on_delete=models.PROTECT)
+    action = models.OneToOneField(Action, on_delete=models.CASCADE)
     old_id = models.IntegerField(unique=True)
