5beb01149703a9fd31c0722736783a231e57876d
Fedjmike <sam.nipps@gmail.com>
2019-05-31 23:18:07 +0100

    Add release and artist views

--- r8music/music/urls.py
+++ r8music/music/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from django.urls import path, reverse
 
+from .views import ArtistPage, ReleasePage
+
 null_view = lambda: None
 
 urlpatterns = [
     path("artists", null_view, name="artist_index"),
     path("tag/<int:pk>", null_view, name="tag"),
-    path("<slug>", null_view, name="artist"),
-    path("<slug:artist_slug>/<slug:release_slug>", null_view, name="release"),
+    path("<slug>", ArtistPage.as_view(), name="artist"),
+    path("<slug:artist_slug>/<slug:release_slug>", ReleasePage.as_view(), name="release"),
 ]
---
+++ r8music/music/views.py
@@ -0,0 +1,18 @@
+#from django.shortcuts import render
+from django.views.generic import DetailView
+
+from .models import Artist, Release
+
+class ArtistPage(DetailView):
+    model = Artist
+    template_name ="artist.html"
+    
+    def get_object(self):
+        return Artist.objects.get(slug=self.kwargs["slug"])
+
+class ReleasePage(DetailView):
+    model = Release
+    template_name ="release.html"
+    
+    def get_object(self):
+        return Release.objects.get(slug=self.kwargs["release_slug"], artists__slug=self.kwargs["artist_slug"])
