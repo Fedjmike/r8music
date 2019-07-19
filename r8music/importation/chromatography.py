903d7c99ae799be0fbf5854a96c0a124f1f34390
Fedjmike <sam.nipps@gmail.com>
2019-07-19 20:57:03 +0100

    Refactor chromatography.py

--- r8music/importation/chromatography.py
+++ r8music/importation/chromatography.py
@@ -23,40 +23,40 @@ class Cluster(object):
         self.sum = initial_color
         self.range = 800
         
-    def fits_in(self, color):
+    def try_add(self, color):
         distance = sq_distance(color, self.get_mean())
-        fits = distance < self.range
+        fits_in = distance < self.range
         
-        if fits:
+        if fits_in:
             self.sum = [p+q for p, q in zip(color, self.sum)]
             self.frequency += 1
             self.range += distance/self.frequency
             
-        return fits
+        return fits_in
         
     def get_mean(self):
         return tuple(int(part/self.frequency) for part in self.sum)
 
 
 class Chromatography(object):
+    sample_size = 10000
+    
     def __init__(self, image_name):
         self.img = Image.open(image_name)
         self.scale_image()
         
+        if self.img.mode in ["1", "L"]:
+            raise BadColorMode("Can't extract colours from black and white image with colour mode " + self.img.mode)
+            
+        elif self.img.mode != "RGB":
+            raise NotImplemented("Not implemented for images with colour mode " + self.img.mode)
+        
     def scale_image(self):
         height, width = self.img.size
-        target_area = 10000
-        ratio = sqrt(height*width/target_area)
+        ratio = sqrt(height*width/self.sample_size)
         self.img = self.img.resize((int(height/ratio), int(width/ratio)), Image.ANTIALIAS)
 
     def get_highlights(self, n=3, valid_color=None):
-        if self.img.mode != "RGB":
-            #B&W, no colours
-            if self.img.mode in ["1", "L"]:
-                raise BadColorMode("Can't extract colours from black and white image with colour mode " + self.img.mode)
-                
-            raise NotImplemented("Not implemented for images with colour mode " + self.img.mode)
-        
         colors = [p for p in self.img.getdata() if valid_color(p)]
         
         if len(colors) == 0:
@@ -65,7 +65,7 @@ class Chromatography(object):
         clusters = []
         
         for color in colors:
-            if not any(c.fits_in(color) for c in clusters):
+            if not any(c.try_add(color) for c in clusters):
                  clusters.append(Cluster(color))
                  
         top_clusters = sorted(clusters, key=lambda c: c.frequency, reverse=True)
@@ -81,7 +81,7 @@ from urllib.request import urlretrieve
 HLS = namedtuple("HLS", ["hue", "lightness", "saturation"])
 rgb_to_hex = lambda color: "#%02x%02x%02x" % color
 rgb_to_hls = lambda color: HLS(*bad_hls(*(c/255 for c in color)))
-    
+
 def hue_difference(pair):
     hues = [rgb_to_hls(c).hue for c in pair]
     diff = abs(hues[0] - hues[1])
@@ -102,13 +102,15 @@ def get_palette(album_art_url):
         try:
             #Select the two colours with hues most different to each other
             most_different = max(combinations(palette, 2), key=hue_difference)
+            
+        except ValueError:
+            pass
+            
+        else:
             #Put the brightest of these second
             most_different = sorted(most_different, key=lambda c: rgb_to_hls(c).lightness)
             #And then any other colours
             palette = most_different + [c for c in palette if c not in most_different]
-            
-        except:
-            pass
             
         if len(palette) < 3:
             palette += [palette[0]] * (3 - len(palette))
